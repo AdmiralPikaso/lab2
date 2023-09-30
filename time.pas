762ee2893ab0c92c13de561c18918a4b5e16d588
Баев Дмитрий <itspodarok@mail.ru>
2023-09-30 11:11:15 +0300

    Modified complited

---
+++ time.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var x:=readinteger('Введите номер года:');
+  if (x mod 4=0) and ((x mod 100<>0) or ((x mod 100=0) and (x mod 400=0))) then print('Весокосный год, в нём 366 дней')
+  else print('Невесокосный, в нём 365 дней');
+end.
---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var (a, b, c) := readinteger3('Введите часы, минуты и секунды:');
+  assert(a > 0);
+  assert(b > 0);
+  assert(c > 0);
+  print(a * 60 * 60 + b * 60 + c);
+end.
